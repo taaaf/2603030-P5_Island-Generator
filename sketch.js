d1d3039573f25eb0a918fe6dc56887e8ec08c813
taaaf <mattia.tafel@gmail.com>
2026-03-31 13:43:22 +0200

    funzione computeAltitude

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
 let PERLIN_SCALE = 200;
-let TILE_SIZE = 32;
+let TILE_SIZE = 10;
+let SPRITE_SIZE = 32;
 
 let grassImage;
 let sandImage;
 let waterImage;
 
+let flowerImage;
+let bushImage;
+
 function preload() {
-  grassImage = loadImage("tiles/grass1.png");
-  sandImage = loadImage("tiles/sand1.png");
-  waterImage = loadImage("tiles/water1.png");
+  grassImage = loadImage("assets/tiles/grass2.png");
+  sandImage = loadImage("assets/tiles/sand1.png");
+  waterImage = loadImage("assets/tiles/water1.png");
+
+  flowerImage = loadImage("assets/sprites/flower.png");
+  bushImage = loadImage("assets/sprites/bush.png");
 }
 
 function setup() {
@@ -19,26 +26,16 @@ function setup() {
   let centralX = width / 2;
   let centralY = height / 2;
 
+  // Tiles
   for (let x = 0; x < width; x = x + TILE_SIZE) {
     for (let y = 0; y < height; y = y + TILE_SIZE) {
-      // Calcolo distanza dal centro
-      let distanceFromCenter = dist(centralX, centralY, x, y);
-      let normDistanceFromCenter = distanceFromCenter / (width / 2); // 0 - 1
-
-      // Calcolo altitudine
-      let altitude = 1 - normDistanceFromCenter;
-
-      // Perlin
-      noiseDetail(6);
-      let perlin = noise(x / PERLIN_SCALE, y / PERLIN_SCALE);
-      altitude *= perlin;
-      altitude += perlin;
-      altitude -= 0.5;
+      let altitude = computeAltitude(x, y, centralX, centralY);
 
       // Assegniamo il colore
       let seaLevel = 0.2;
       let beachLevel = 0.28;
 
+      // Tiles
       let img;
       if (altitude < seaLevel) {
         img = waterImage;
@@ -47,8 +44,42 @@ function setup() {
       } else {
         img = grassImage;
       }
-
       image(img, x, y, TILE_SIZE, TILE_SIZE);
     }
   }
+
+  // Sprites
+  for (let x = 0; x < width; x = x + TILE_SIZE) {
+    for (let y = 0; y < height; y = y + TILE_SIZE) {
+      let altitude = computeAltitude(x, y, centralX, centralY);
+
+      // Assegniamo il colore
+      let seaLevel = 0.2;
+      let beachLevel = 0.28;
+
+      // Flower
+      let flow;
+      if (random() < 0.1 && altitude > beachLevel) {
+        image(flowerImage, x, y, SPRITE_SIZE, SPRITE_SIZE);
+      }
+    }
+  }
+}
+
+function computeAltitude(x, y, centralX, centralY) {
+  // Calcolo distanza dal centro
+  let distanceFromCenter = dist(centralX, centralY, x, y);
+  let normDistanceFromCenter = distanceFromCenter / (width / 2); // 0 - 1
+
+  // Calcolo altitudine
+  let altitude = 1 - normDistanceFromCenter;
+
+  // Perlin
+  noiseDetail(6);
+  let perlin = noise(x / PERLIN_SCALE, y / PERLIN_SCALE);
+  altitude *= perlin;
+  altitude += perlin;
+  altitude -= 0.5;
+
+  return altitude;
 }
