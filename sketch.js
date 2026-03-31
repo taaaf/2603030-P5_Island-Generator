2b21a3487df630ab1eb4c90f9662e323af01b981
taaaf <mattia.tafel@gmail.com>
2026-03-31 10:41:29 +0200

    colori isola

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let PERLIN_SCALE = 50;
+
 function setup() {
   createCanvas(200, 200);
 
@@ -13,9 +15,25 @@ function setup() {
       let distanceFromCenter = dist(centralX, centralY, x, y);
       let normDistanceFromCenter = distanceFromCenter / (width / 2); // 0 - 1
 
+      // Calcolo altitudine
       let altitude = 1 - normDistanceFromCenter;
 
-      fill(altitude * 255);
+      // Perlin
+      noiseDetail(6);
+      let perlin = noise(x / PERLIN_SCALE, y / PERLIN_SCALE);
+      altitude *= perlin;
+
+      // Assegniamo il colore
+      let seaLevel = 0.2;
+      let beachLevel = 0.23;
+      if (altitude < seaLevel) {
+        fill(0, 0, 255);
+      } else if (altitude < beachLevel) {
+        fill(255, 255, 0);
+      } else {
+        fill(0, 255, 0);
+      }
+
       rect(x, y, 1, 1);
     }
   }
