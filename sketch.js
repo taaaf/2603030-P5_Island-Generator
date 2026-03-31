4bf5cb8bee33c05280d7c326397caf07f09c2273
taaaf <mattia.tafel@gmail.com>
2026-03-31 11:49:22 +0200

    tiles

--- sketch.js
+++ sketch.js
@@ -1,16 +1,26 @@
-let PERLIN_SCALE = 50;
+let PERLIN_SCALE = 200;
+let TILE_SIZE = 32;
+
+let grassImage;
+let sandImage;
+let waterImage;
+
+function preload() {
+  grassImage = loadImage("tiles/grass1.png");
+  sandImage = loadImage("tiles/sand1.png");
+  waterImage = loadImage("tiles/water1.png");
+}
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(windowWidth, windowHeight);
 
-  background(255, 0, 0);
   noStroke();
 
   let centralX = width / 2;
   let centralY = height / 2;
 
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
+  for (let x = 0; x < width; x = x + TILE_SIZE) {
+    for (let y = 0; y < height; y = y + TILE_SIZE) {
       // Calcolo distanza dal centro
       let distanceFromCenter = dist(centralX, centralY, x, y);
       let normDistanceFromCenter = distanceFromCenter / (width / 2); // 0 - 1
@@ -22,19 +32,23 @@ function setup() {
       noiseDetail(6);
       let perlin = noise(x / PERLIN_SCALE, y / PERLIN_SCALE);
       altitude *= perlin;
+      altitude += perlin;
+      altitude -= 0.5;
 
       // Assegniamo il colore
       let seaLevel = 0.2;
-      let beachLevel = 0.23;
+      let beachLevel = 0.28;
+
+      let img;
       if (altitude < seaLevel) {
-        fill(0, 0, 255);
+        img = waterImage;
       } else if (altitude < beachLevel) {
-        fill(255, 255, 0);
+        img = sandImage;
       } else {
-        fill(0, 255, 0);
+        img = grassImage;
       }
 
-      rect(x, y, 1, 1);
+      image(img, x, y, TILE_SIZE, TILE_SIZE);
     }
   }
 }
